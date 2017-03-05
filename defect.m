419e1f621fbc6d9247856df48a328bb80626cd82
xxx-matrix <xxx-matrix@users.noreply.github.com>
2017-03-05 22:23:31 +0000

    Add files via upload

---
+++ defect.m
@@ -0,0 +1,76 @@
+% 20170304
+% [dephased] defect of a square matrix
+
+function d = defect(U, METHOD, SV_TOLERANCE)
+
+    if ~exist('U') || ~ismatrix(U) || size(U, 1) ~= size(U, 2)
+        error('No square matrix provided!');
+    end
+
+    N = size(U, 1);
+    if N > 128
+        warning(sprintf('Matrix size is %d. Higher orders take longer...', N));
+    end
+
+    try
+        matrix_type = get_matrix_type(U);
+    catch
+        error('Unsupported matrix type!');
+        return
+    end
+
+    % argument validator
+    POSSIBLE_METHOD = ['R', 'S', 'T'];
+    if ~exist('METHOD') || ~any(ismember(POSSIBLE_METHOD, METHOD))
+        disp('No valid ''METHOD'' provided when calling ''defect(MATRIX [, METHOD [, SV_TOLERANCE]])''.');
+        disp('Possible values:');
+        disp('METHOD: ''R'' - rank of matrix ''R''.');
+        disp('METHOD: ''S'' - number of non-zero singular values of ''R''.');
+        disp('METHOD: ''T'' - dimension of tangent space...');
+        disp('Attempt to calculate defect using the rank of ''R'' matrix.');
+        METHOD = 'R';
+    end
+    if ~exist('SV_TOLERANCE') || ~isnumeric(SV_TOLERANCE)
+        SV_TOLERANCE = 1e-13;
+    end
+
+    switch matrix_type
+        case 'unitary'
+            disp('Input matrix is (only) unitary.');
+            disp('Continue with STANDARD defect procedure.');
+            d = defect_u(U, METHOD, SV_TOLERANCE);
+        case 'unitary_hermitian'
+            disp('Input matrix is unitary and hermitian.');
+            disp('Continue with RESTRICTED defect procedure.');
+            d = defect_h(U, METHOD, SV_TOLERANCE);
+        otherwise
+            error('Not implemented...');
+    end
+end
+
+function matrix_type = get_matrix_type(U)
+    if is_unitary(U)
+        matrix_type = 'unitary';
+        if is_hermitian(U) % in-built "ishermitian" function is not sensitive enough...
+            matrix_type = 'unitary_hermitian';
+        end
+    end
+end
+
+function b = is_hermitian(U)
+    b = false;
+    T = abs(U - U');
+    if sum(T(:)) < 1e-10 % note - the tolerance is relatively small...
+        b = true;
+    end
+end
+
+function b = is_unitary(U)
+    b = false;
+    T = U * U';
+    T = T / T(1, 1); % U = unitary => T ~ I
+    if norm(T - eye(size(T, 1)), 'fro') < 1e-10 % note - the tolerance is relatively small...
+        b = true;
+    end
+end
+
---
+++ defect_h.m
@@ -0,0 +1,75 @@
+% 20170209
+% [Restricted] defect of a unitary matrix U subjected to additional constraints.
+
+function d = defect_h(U, METHOD, SV_TOLERANCE)
+
+    N = size(U, 1);
+    disp('Wait...');
+    z = 0; % number of zeros above the diagonal in U
+    for j = 1 : N - 1
+        for k = j + 1 : N
+            z = z + (abs(U(j, k)) < 1e-12);
+        end
+    end
+    % z = MUBs * N * (N - 1) / 2 for MUBs
+
+    tau = N * (N - 1) / 2;
+    trivial_phases = N - 1; % see equivalence relation...
+    R = zeros(tau * 2, tau); % system of linear equations matrix split onto real and imaginary part 
+    index = @(j, k) (j - 1) * N + k - j - j * (j - 1) / 2;
+    next_row = -1;
+    for j = 1 : N - 1
+        for k = j + 1 : N
+            next_row = next_row + 2; % start with index = 1
+            Ukk_Ujk = U(k, k) * U(j, k)';
+            R(next_row + 0, index(j, k)) = - 2 * real(Ukk_Ujk);
+            R(next_row + 1, index(j, k)) = - 2 * imag(Ukk_Ujk);
+            for l = 1 : N
+                if (l ~= j && l ~= k)
+                    if (k < l)
+                        Ukl = U(k, l);
+                        index_kl = index(k, l);
+                    else
+                        Ukl = U(l, k)';
+                        index_kl = index(l, k);
+                    end
+                    if (j < l)
+                        Ujl = U(j, l)';
+                        index_jl = index(j, l);
+                    else
+                        Ujl = U(l, j);
+                        index_jl = index(l, j);
+                    end
+                    Ukl_Ujl = Ukl * Ujl;
+                    UR = real(Ukl_Ujl);
+                    UI = imag(Ukl_Ujl);
+                    R(next_row + 0, index_kl) = UR * sign(l - k);
+                    R(next_row + 1, index_kl) = UI * sign(l - k);
+                    R(next_row + 0, index_jl) = UR * sign(j - l);
+                    R(next_row + 1, index_jl) = UI * sign(j - l);
+                end
+            end
+        end
+    end
+
+    % [U S V] = svd(R); 
+    % svd(R)
+    % sum_of_S = sum(S.^2)
+    % trace_R = trace(R'*R)
+    % return
+
+    switch METHOD
+        case 'S'
+            disp(sprintf('Method: ''S'' with ''SV_TOLERANCE'' = %g', SV_TOLERANCE));
+            disp(sprintf('Wait... Getting SVD of ''R'''));
+            NZSV_R = sum((svd(R) > SV_TOLERANCE)); % number of non-zero singular values of R
+            d = tau - NZSV_R - trivial_phases - z;
+        case 'R'
+            disp(sprintf('Method: ''R'''));
+            disp(sprintf('Wait... Getting rank of ''R'''));
+            d = tau - rank(R) - trivial_phases - z;
+        otherwise
+            error('Not implemented!');
+    end
+end
+
