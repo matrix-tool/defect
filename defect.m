3f3243e29a8b7009c1e42bc0f81be3c60e4e31a3
matrix-toolbox <matrix-toolbox@users.noreply.github.com>
2017-03-07 10:54:03 +0000

    Update defect.m

--- defect.m
+++ defect.m
@@ -1,6 +1,6 @@
 % 20170304
+% name[at]alumni.uj.edu.pl where name = w.bruzda
 % https://github.com/matrix-tool/defect
-% name[at]alumni.uj.edu.pl, name = w.bruzda
 
 % [dephased] defect of a square matrix
 
@@ -85,4 +85,3 @@ function b = is_unitary(U)
         b = true;
     end
 end
-
